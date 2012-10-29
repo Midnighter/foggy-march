00e3ca77febcf99b160073b6aaa3aef6586bf788
Moritz Emanuel Beber <m.beber@jacobs-university.de>
2012-10-29 22:49:57 +0100

    amended readme

--- walkers.py
+++ walkers.py
@@ -21,6 +21,7 @@ Random Walkers on Networks
 import sys
 import itertools
 import bisect
+import multiprocessing
 import numpy
 import networkx as nx
 
@@ -47,7 +48,7 @@ class UniformInterval(object):
         return self.mid_point + self.rand_int(-self.variation, self.variation)
 
 
-def uniform_random_walk((graph, node, steps)):
+def uniform_random_walk((adj, node, steps)):
     """
     Perform a single random walk on a network with a uniform probability of a
     next step.
@@ -65,28 +66,36 @@ def uniform_random_walk((graph, node, steps)):
     choose = bisect.bisect_left
     path = [node]
     for s in xrange(steps):
-        attr = graph.node[node]
-        if len(attr["nbrs"]) == 0:
+        (nbrs, probs) = adj.get(node, (False, False))
+        if not nbrs:
             return path
         draw = smpl()
-        node = attr["nbrs"][choose(attr["probs"], draw)]
+        # the nbrs list and probs list correspond to each other
+        # we use a binary search to find the index to the left of the
+        # probability and take that node
+        node = nbrs[choose(probs, draw)]
         path.append(node)
     return path
 
-def prepare_uniform_walk_graph(graph, weight=None):
-    for (node, attr) in graph.nodes_iter(data=True):
-        attr["nbrs"] = list()
-        attr["probs"] = list()
+def prepare_uniform_walk_adjacency(graph, weight=None):
+    adj = dict()
+    for node in graph.nodes_iter():
+        neighbours = list()
+        probabilities = list()
         if graph.out_degree(node) == 0:
             continue
-        factor = 1 / float(graph.out_degree(node))
         prob = 0.0
         for (u, v, data) in graph.out_edges_iter(data=True):
-            attr["nbrs"].append(v)
-            prob += data.get(weight, 1.0) * factor
-            attr["probs"].append(prob)
+            neighbours.append(v)
+            prob += data.get(weight, 1.0)
+            probabilities.append(prob)
+        probabilities = numpy.asarray(probabilities)
+        # prob is now the sum of all edge weights, normalise to unity
+        probabilities /= prob
+        adj[node] = (neighbours, probabilities)
+    return adj
 
-def walks(graph, num_walkers, time_points, steps, weight=None):
+def walks(graph, num_walkers, time_points, steps, weight=None, num_cpu=1):
     """
     Start a number of random walkers on the given network for given time points.
     """
@@ -94,27 +103,42 @@ def walks(graph, num_walkers, time_points, steps, weight=None):
     length = len(nodes)
     if length < 2:
         raise nx.NetworkXError("network is too small")
+    time_points = int(time_points)
+    steps = int(steps)
+    num_cpu = int(num_cpu)
     rand_int = numpy.random.randint
-    visits = numpy.zeros(shape=(length, time_points), dtype="int32")
+    adjacency = prepare_uniform_walk_adjacency(graph, weight)
+    # compute a running mean and sd as per:
+    # http://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
+    visits = numpy.zeros(length, dtype="int32")
+    mean_fluxes = numpy.zeros(length)
+    subtraction = numpy.zeros(length)
+    std_fluxes = numpy.zeros(length)
     indices = dict(itertools.izip(nodes, itertools.count()))
-    for t in xrange(time_points):
-        print "\rtime", t,
-# this step lends itself to a multiprocessing result iterator
-# in that case we can iteratively compute the mean and sd of the flux and save
-# memory space
-        visited = map(uniform_random_walk,
-                [(graph, nodes[rand_int(length)], steps)\
+    pool = multiprocessing.Pool(num_cpu)
+    for time in xrange(1, time_points + 1):
+        sys.stdout.write("\r{0:.2%} completed".format(
+            time / float(time_points)))
+        sys.stdout.flush()
+        visits.fill(0)
+        visited = pool.imap(uniform_random_walk,
+                [(adjacency, nodes[rand_int(length)], steps)\
                 for i in xrange(num_walkers())])
         for path in visited:
             for node in path:
-                visits[indices[node], t] += 1
-    print
-    return visits
+                visits[indices[node]] += 1
+        # compute running average and variation
+        subtraction = visits - mean_fluxes
+        mean_fluxes += subtraction / time
+        std_fluxes += subtraction * (visits - mean_fluxes)
+    std_fluxes /= time - 1
+    std_fluxes = numpy.sqrt(std_fluxes)
+    sys.stdout.write("\n")
+    return (mean_fluxes, std_fluxes)
 
 def main(argv):
-    net = nx.scale_free_graph(1E04)
-    prepare_uniform_walk_graph(net)
-    print walks(net, UniformInterval(1E04, 20), 1E02, 1E03)
+    net = nx.scale_free_graph(1E01)
+    print walks(net, UniformInterval(1E02, 20), 1E02, 1E03)
 
 
 if __name__ == "__main__":

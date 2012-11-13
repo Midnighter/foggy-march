c5d9685d983ee39b2e74014e39f04c07e7fa7a9e
Moritz Emanuel Beber <m.beber@jacobs-university.de>
2012-11-13 14:16:49 +0100

    completely queue- and array-based random walks

--- walkers.py
+++ walkers.py
@@ -18,13 +18,11 @@ Random Walkers on Networks
 
 
 import sys
-#import itertools
+import itertools
 import bisect
 import multiprocessing
 import numpy
 import networkx as nx
-
-from Queue import Empty
 
 
 class UniformInterval(object):
@@ -34,6 +32,8 @@ class UniformInterval(object):
         self.rand_int = numpy.random.random_integers
         self.mid_point = int(mid_point)
         self.variation = int(variation)
+        self.mini = self.mid_point - self.variation
+        self.maxi = self.mid_point + self.variation
 
     def __call__(self):
         if self.variation > 0:
@@ -46,100 +46,45 @@ class UniformInterval(object):
         return self.mid_point
 
     def variable(self):
-        return self.mid_point + self.rand_int(-self.variation, self.variation)
+        return self.rand_int(self.mini, self.maxi)
 
 
-def uniform_random_walk((adj, node, steps)):
+def prepare_uniform_walk(graph, weight=None):
     """
-    Perform a single random walk on a network with a uniform probability of a
-    next step.
+    Prepare data structures for a directed uniform random walk.
 
     Arguments
     ---------
     graph: nx.DiGraph
         The underlying network.
-    node: hashable
-        A node from the network.
-    steps: int
-        A positive number of steps the walker should go until it is removed.
+    weight: hashable
+        The keyword for edge data that should be used to weigh the propagation
+        probability.
     """
-    smpl = numpy.random.random_sample
-    choose = bisect.bisect_left
-    path = [node]
-    for s in xrange(steps):
-        (nbrs, probs) = adj.get(node, (False, False))
-        if not nbrs:
-            return path
-        draw = smpl()
-        # the nbrs list and probs list correspond to each other
-        # we use a binary search to find the index to the left of the
-        # probability and take that node
-        node = nbrs[choose(probs, draw)]
-        path.append(node)
-    return path
-
-def prepare_uniform_walk_adjacency(graph, weight=None):
     nodes = sorted(graph.nodes())
     if len(nodes) < 2:
         raise nx.NetworkXError("network is too small")
-    adj = dict()
-    indices = dict()
+    indices = dict(itertools.izip(nodes, itertools.count()))
+    probabilities = list()
+    neighbours = list()
     for (i, node) in enumerate(nodes):
-        indices[node] = i
-        neighbours = list()
-        probabilities = list()
-        if graph.out_degree(node) == 0:
+        out_deg = graph.out_degree(node)
+        if out_deg == 0:
+            probabilities.append([])
+            neighbours.append([])
             continue
         prob = 0.0
-        for (u, v, data) in graph.out_edges_iter(data=True):
-            neighbours.append(v)
+        probabilities.append(numpy.zeros(out_deg, dtype=float))
+        neighbours.append(numpy.zeros(out_deg, dtype=int))
+        for (j, (u, v, data)) in enumerate(graph.out_edges_iter(data=True)):
+            neighbours[i][j] = indices[v]
             prob += data.get(weight, 1.0)
-            probabilities.append(prob)
-        probabilities = numpy.asarray(probabilities)
+            probabilities[i][j] = prob
         # prob is now the sum of all edge weights, normalise to unity
         probabilities /= prob
-        adj[node] = (neighbours, probabilities)
-    return (adj, indices)
+    return (probabilities, neighbours)
 
-def walks_pool(adjacency, indices, num_walkers, time_points, steps, num_cpu=1):
-    """
-    Start a number of random walkers on the given network for given time points.
-    """
-    time_points = int(time_points)
-    steps = int(steps)
-    num_cpu = int(num_cpu)
-    nodes = sorted(indices.keys())
-    length = len(nodes)
-    rand_int = numpy.random.randint
-    # compute a running mean and sd as per:
-    # http://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
-    visits = numpy.zeros(length, dtype="int32")
-    mean_fluxes = numpy.zeros(length)
-    subtraction = numpy.zeros(length)
-    std_fluxes = numpy.zeros(length)
-    pool = multiprocessing.Pool(num_cpu)
-    for time in xrange(1, time_points + 1):
-        sys.stdout.write("\r{0:.2%} completed".format(
-            time / float(time_points)))
-        sys.stdout.flush()
-        visits.fill(0)
-        # a large number of walkers will probably exceed available memory
-        visited = pool.imap(uniform_random_walk,
-                [(adjacency, nodes[rand_int(length)], steps)\
-                for i in xrange(num_walkers())])
-        for path in visited:
-            for node in path:
-                visits[indices[node]] += 1
-        # compute running average and variation
-        subtraction = visits - mean_fluxes
-        mean_fluxes += subtraction / time
-        std_fluxes += subtraction * (visits - mean_fluxes)
-    std_fluxes /= time - 1
-    std_fluxes = numpy.sqrt(std_fluxes)
-    sys.stdout.write("\n")
-    return (mean_fluxes, std_fluxes)
-
-def uniform_random_walk_worker(in_queue, out_queue, adjacency):
+def uniform_random_walker(in_queue, out_queue, probabilities, neighbours):
     """
     Perform a single random walk on a network with a uniform probability of a
     next step.
@@ -152,7 +97,7 @@ def uniform_random_walk_worker(in_queue, out_queue, adjacency):
     for (node, steps) in iter(in_queue.get, "STOP"):
         path = [node]
         for s in xrange(steps):
-            (nbrs, probs) = adjacency.get(node, (False, False))
+            nbrs = neighbours[node]
             if not nbrs:
                 out_queue.put(path)
                 continue
@@ -160,11 +105,12 @@ def uniform_random_walk_worker(in_queue, out_queue, adjacency):
             # the nbrs list and probs list correspond to each other
             # we use a binary search to find the index to the left of the
             # probability and take that node
-            node = nbrs[choose(probs, draw)]
+            node = nbrs[choose(probabilities[node], draw)]
             path.append(node)
         out_queue.put(path)
 
-def walks_queue(adjacency, indices, num_walkers, time_points, steps, num_cpu=1):
+def queued_march(neighbours, probabilities, indices, num_walkers, time_points,
+        steps, num_cpu=1):
     """
     Start a number of random walkers on the given network for given time points.
     """
@@ -184,8 +130,8 @@ def walks_queue(adjacency, indices, num_walkers, time_points, steps, num_cpu=1):
     in_queue = multiprocessing.Queue()
     out_queue = multiprocessing.Queue()
     # start processes
-    workers = [multiprocessing.Process(target=uniform_random_walk_worker,
-            args=(in_queue, out_queue, adjacency))\
+    workers = [multiprocessing.Process(target=uniform_random_walker,
+            args=(in_queue, out_queue, probabilities, neighbours))\
             for i in range(num_cpu)]
     for proc in workers:
         proc.daemon = True
